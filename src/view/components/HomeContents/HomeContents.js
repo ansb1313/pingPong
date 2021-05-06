74b4fdaabe5b456b2491e06cf62cc3adf06844cc
ahn <ansb1029@gmail.com>
2021-05-07 01:52:28 +0900

    second

--- src/view/components/HomeContents/HomeContents.js
+++ src/view/components/HomeContents/HomeContents.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import HomeTemplate from './HomeTemplate'
+
 import SectionPage1 from './Section/SectionPage1'
 import SectionPage2 from './Section/SectionPage2'
 import SectionPage3 from './Section/SectionPage3'
@@ -9,7 +9,6 @@ const HomeContents = () => {
     
     return(
         <Container>
-            <HomeTemplate/>
             <SectionPage1/>
             <SectionPage2/>
             <SectionPage3/>
@@ -21,8 +20,8 @@ const Container = styled.div`
     box-sizing: border-box;
     background: #eee;
     position: relative;
-    height: 300%;
-    width: 100vw;
+    height: 300vh;
+    overflow-x: hidden;
 `
 
 export default HomeContents
